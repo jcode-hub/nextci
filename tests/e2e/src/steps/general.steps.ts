e886c993d2da89d3447137c81d8a41652b9ac1a6
Janek <jhornung@oev.de>
2022-03-07 21:02:14 +0100

    chore: -nocursor

--- tests/e2e/src/steps/general.steps.ts
+++ tests/e2e/src/steps/general.steps.ts
@@ -23,8 +23,8 @@ Then(
   async function (this: ICustomWorld, name: string, timeout: number) {
     const page = this.page!;
     await page.goto(config.BASE_URL);
-    await this.page?.waitForTimeout(timeout * 1000);
     await page.mouse.move(0, 0);
+    await this.page?.waitForTimeout(timeout * 1000);
     const screenshot = await this.page!.screenshot();
     await compareToBaseSnapshot(screenshot as Buffer, `${name}TO`, this);
   }
